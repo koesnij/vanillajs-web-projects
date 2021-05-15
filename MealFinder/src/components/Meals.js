97eab508e315e61bd2ecc219db4121b7ea5750b2
문진석 <m_jinseok@naver.com>
2021-05-15 21:29:24 +0900

    [add] MealFinder/src/components/Meals.js

---
+++ MealFinder/src/components/Meals.js
@@ -0,0 +1,37 @@
+export default class Meals {
+  constructor({ $target, initialState, onClick }) {
+    this.$target = $target;
+    this.state = {
+      meals: initialState,
+    };
+    this.onClick = onClick;
+
+    this.init();
+    this.render();
+  }
+
+  init() {
+    this.$target.onclick = this.onClick;
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = this.state.meals
+      .map((meal) => {
+        const { idMeal, strMeal, strMealThumb } = meal;
+        return `
+          <div class="meal">
+            <img src="${strMealThumb}" alt="${strMeal}">
+            <div class="meal-info" data-mealid="${idMeal}">
+              <h3>${strMeal}</h3>
+            </div>
+          </div>
+        `;
+      })
+      .join('');
+  }
+}
